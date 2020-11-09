a4f0fb1351d78473c16e7f363e4dea375475d8be
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2020-11-09 13:55:22 -0600

    set up states table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print(f"Welcome to Ndeed")
 
-sign_in_up = ["sign in", "sign up", "sign out"]
+sign_in_options = ["sign in", "sign up", "sign out"]
 sign_in = input("""
     Do you want to
         - Sign in
@@ -8,74 +8,26 @@ sign_in = input("""
     """)
     
 
-while sign_in not in sign_in_up:
+while sign_in not in sign_in_options:
     print("Not a valid option")
     sign_in = input("""
-    Do you want to
-        - Sign in
-        - Sign up
-    """
-    )
-["Alabama",
-"Alaska",
-"Arizona",
-"Arkansas"
-"California",
-"Colorado",
-"Connecticut",
-"Delaware",
-"Florida",
-"Georgia",
-"Hawaii",
-"Idaho",
-"Illinois",
-"Indiana",
-"Iowa",
-"Kansas",
-"Kentucky",
-"Louisiana",
-"Maine",
-"Maryland",
-"Massachusetts",
-"Michigan",
-"Minnesota",
-"Mississippi",
-"Missouri",
-"Montana",
-"Nebraska",
-"Nevada",
-"New Hampshire",
-"New Jersey",
-"New Mexico",
-"New York",
-"North Carolina",
-"North Dakota",
-"Ohio",
-"Oklahoma",
-"Oregon",
-"Pennsylvania",
-"Rhode Island",
-"South Carolina",
-"South Dakota",
-"Tennessee",
-"Texas",
-"Utah",
-"Vermont",
-"Virginia",
-"Washington",
-"West Virginia",
-"Wisconsin",
-"Wyoming"]
+        Do you want to
+            - Sign in
+            - Sign up
+        """)
+
 if sign_in == "sign in":
     print("signed in")
 elif sign_in == "sign up":
     name = input("Full name: ")
 
-    age = input("Age: ")
-    if age.isdigit():
-        age = int(age)
-    else:
-        print("Please enter a numerical value.")
+    while True:
+        age = input("Age: ")
+        if age.isdigit():
+            age = int(age)
+            break
+        else:
+            print("Please enter a numerical value.")
     email = input("Email: ")
 
     cell = input("Phone number: ")
@@ -85,4 +37,4 @@ elif sign_in == "sign up":
     gender = input("Gender: ")
 
 
-print(2+2)
+
--- storing.py
+++ storing.py
@@ -9,9 +9,14 @@ try:
     cur.execute('CREATE TABLE Person (Name TEXT, Age INTEGER, Email TEXT, Phone_number TEXT, Place TEXT, Job TEXT, Gender TEXT')
 except:
     pass
-
+ADMIN = []
 with open("admin.json") as admin_file:
-    ADMIN = json.load(admin_file)
+    READER = json.load(admin_file)
+    for entry in READER:
+        ADMIN.append(entry["name"])
+
+
+
 
 class Admin:
     def __init__(self, name):
@@ -20,4 +25,77 @@ class Admin:
 
     def __str__(self):
         return self.name
+
+print(ADMIN)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cur.execute("CREATE TABLE IF NOT EXISTS States (name TEXT, abbreviation TEXT)")
+states = [("Alabama", "AL"),
+("Alaska", "AK"),
+("Arizona", "AZ"),
+("Arkansas", "AR"),
+("California", "CA"),
+("Colorado", "CO"), 
+("Connecticut", "CT"),
+("Delaware", "DE"),
+("Florida", "FL"),
+("Georgia", "GA"),
+("Hawaii", "HI"),
+("Idaho", "ID"),
+("Illinois", "IL"),
+("Indiana", "IN"),
+("Iowa", "IA"),
+("Kansas", "KS"),
+("Kentucky", "KY"),
+("Louisiana", "LA"),
+("Maine", "ME"),
+("Maryland", "MD"),
+("Massachusetts", "MA"),
+("Michigan", "MI"),
+("Minnesota", "MN"),
+("Mississippi", "MS"),
+("Missouri", "MO"),
+("Montana", "MT"),
+("Nebraska", "NE"),
+("Nevada", "NV"),
+("New Hampshire", "NH"),
+("New Jersey", "NJ"),
+("New Mexico", "NM"),
+("New York", "NY"),
+("North Carolina", "NC"),
+("North Dakota", "ND"),
+("Ohio", "OH"),
+("Oklahoma", "OK"),
+("Oregon",  "OR"),
+("Pennsylvania", "PA"),
+("Rhode Island", "RI"),
+("South Carolina", "SC"),
+("South Dakota", "SD"),
+("Tennessee", "TN"),
+("Texas", "TX"),
+("Utah", "UT"),
+("Vermont", "VT"),
+("Virginia", "VA"),
+("Washington", "WA"),
+("West Virginia", "WV"),
+("Wisconsin", "WI"),
+("Wyoming", "WY")]
 
