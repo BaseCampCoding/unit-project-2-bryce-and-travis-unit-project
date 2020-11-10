e3e9f22e3daec42aa7cc9db5a5d5cb4eba9a8411
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2020-11-10 09:43:16 -0600

    fixing input validation

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import storing
 from storing import *
 
+
+
+def customer():
+    check job listings
+    pass
+
+def admin():
+    customer()
+
+    
+
+
+
 print(f"Welcome to Ndeed")
 
 sign_in_options = ["sign in", "sign up", "sign out", "admin"]
@@ -25,6 +38,7 @@ if sign_in == "sign in":
     p = input("Password: ")
     log = (n, p)
     print("signed in")
+    customer()
 
 elif sign_in == "admin":
     print("Enter your Admin account")
@@ -33,7 +47,7 @@ elif sign_in == "admin":
     account = Admin([name, password])
     if account.is_valid:
         print(f"Welcome{name}")
-        break
+        admin()
     else:
         print("Invaild account")
     
@@ -50,12 +64,11 @@ elif sign_in == "sign up":
     email = input("Email: ")
 
     while True:    
-        cell = input("Phone number: ")
+        cell = input("Phone number: (digits only) ")
         if cell.isdigit() and len(list(cell)) == 10:
             cell = int(cell)
             break
         else:
-            
             print("Please enter a 10 digit cell phone number.")
     
     place = input("What state do you live in: ").lower()
@@ -74,7 +87,5 @@ elif sign_in == "sign up":
     password = input("Please enter a password for your account: ")
 
 
-cur.execute('INSERT INTO Person VALUES (?, ?, ?, ?, ?, ?, ?)', (name, age, email, str(cell), place, job, gender))
-cur.execute('SELECT * FROM Person')
-for row in cur.fetchall():
-    print(row)
+    cur.execute('INSERT INTO Person VALUES (?, ?, ?, ?, ?, ?, ?)', (name, age, email, str(cell), place, job, gender))
+
