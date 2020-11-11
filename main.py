0bfbc17fa2d40e9a99ba07cf603330465cce04ed
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2020-11-11 13:55:57 -0600

    added admin ability to see users

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import PySimpleGUI as gui
 
 def Employee():
     
-    with open('jobs.json') as json_file:
-        JOB_LIST = json.load(json_file)
 
     user_options = ["view jobs", "update profile"]
     choice = input("""
@@ -53,8 +51,7 @@ def Employee():
 
 def admin():
     while True:
-        with open('jobs.json') as json_file:
-            JOB_LIST = json.load(json_file)
+
         admin_options = ["view jobs", "view users", "add jobs", "delete jobs", "sign out"]
         admin_input = input("""
         Do you want to
@@ -85,10 +82,10 @@ def admin():
         
         elif admin_input == "view users":
             yn = ["y", "yes", "n", "no", "quit"]
-            choices = input("Do you want to see a list of ALL users?").lower()
+            choices = input("Do you want to see a list of ALL users? [y/n] " ).lower()
             while choices not in yn:
                 print(f"{choices} is not a valid answer")
-                choices = input("Do you want to see a list of ALL users?").lower()
+                choices = input("Do you want to see a list of ALL users? [y/n] ").lower()
                 if choices in yn:
                     break
             if choices == "y" or choices == "yes":
@@ -96,6 +93,19 @@ def admin():
                 all_users = cur.fetchall()
                 for user in all_users:
                     print(user[0])
+            elif choices == "n" or choices == "no":
+                pass
+
+            while True:    
+                one_user = input("What user so you want information for? ")
+                cur.execute('SELECT name FROM person WHERE name = ?', (one_user,))
+                isuser = cur.fetchall()
+                if isuser == []:
+                    print(f"We don't have a user by the name of {one_user}.")
+                elif isuser != []:
+                    cur.execute('SELECT * FROM person WHERE name = ?', (one_user,))
+                    print(cur.fetchone())
+
                 
         
         elif admin_input == "sign out":
@@ -169,7 +179,7 @@ while True:
     
     elif sign_in == "sign up":
         while True:
-            name = input("Full name: ")
+            name = input("Full name: ").lower()
             if name == "quit":
                 break
             while True:
@@ -230,12 +240,14 @@ while True:
                 break
             if gender != "male" and gender != "female":
                 gender = "other"
-
-            username = input("\nPlease enter in a username: ")
-            cur.execute('SELECT username FROM log_in WHERE username = ?', (username,))
-            cur.fetchall()
-            if username != []:
-                print("That username is already being used!")
+            while True:
+                username = input("\nPlease enter in a username: ")
+                cur.execute('SELECT username FROM log_in WHERE username = ?', (username,))
+                username_list = cur.fetchall()
+                if username_list != []:
+                    print("That username is already being used!")
+                elif username_list == []:
+                    break
             
             password = input("Please enter a password for your account: ")
             if password == "quit":
